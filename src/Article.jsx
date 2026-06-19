54ce6618701309238d4c5bd0d84ff740a60d9ca6
Roman <Romangorbunov4890@gmail.com>
2026-06-19 13:08:15 +0400

    add article rendering

--- src/Article.jsx
+++ src/Article.jsx
@@ -1,15 +1,20 @@
+import {useParams} from "react-router-dom";
+import loadArticles,  {trimArticleName} from "./articleLoader.js";
+import {useEffect, useState} from "react";
 
-function getArticleContent(article) {
-    return article
+function getArticleContent(articleName) {
+    const articles = loadArticles()
+    const articleKey = Object.keys(articles).find(article => trimArticleName(article) === articleName)
+    return articles[articleKey]()
 }
 
 function Article() {
+    let articleName = useParams().articleName;
+    let articleContent = getArticleContent(articleName);
+    const [text, setText] = useState("Loading...")
+    useEffect(() => {articleContent.then(result => {setText(result)})}, [articleContent, articleName]);
     return(
-        <div>
-
-
-
-        </div>
+    <div> {text}</div>
     )
 }
 export default Article;
